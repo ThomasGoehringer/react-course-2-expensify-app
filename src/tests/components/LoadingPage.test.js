7eea7dbd4d8b1d962020d4fabb5c66b30a5d6652
Thomas Göhringer <thomas.goehringer@wuerth-it.com>
2019-07-12 19:40:21 +0200

    Add Expensify styles

---
+++ src/tests/components/LoadingPage.test.js
@@ -0,0 +1,10 @@
+/* eslint-env jest */
+
+import React from 'react'
+import { shallow } from 'enzyme'
+import LoadingPage from '../../components/LoadingPage'
+
+test('should render LoadingPage correctly', () => {
+  const wrapper = shallow(<LoadingPage />)
+  expect(wrapper).toMatchSnapshot()
+})
